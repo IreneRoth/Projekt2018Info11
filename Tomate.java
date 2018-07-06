8bfac728b3ee93fe2f523b8b27b19478259bf971
Jasper <irene.roth@derksen-gym.de>
2018-07-06 09:13:44 +0200

    grosseKarotte und Tomate eingefügt

---
+++ Tomate.java
@@ -0,0 +1,34 @@
+import ch.aplu.jgamegrid.*;
+import java.awt.Color;
+
+public class Tomate extends Geschoss 
+{
+    private int count=0;
+    public Tomate(){
+        super ("Bilder/tomate.png",2);
+        show();
+    }
+
+    public void act() {    
+        count++;
+        if(count == 10){
+            count=0;
+            showNextSprite();
+        }
+
+    }
+
+    public void geheZu(int x,int y) {
+        Location location=gameGrid.toLocationInGrid(x,y);
+        setLocation(location);
+        System.out.println("CLICK" + x + " " + y);
+    }
+
+    public void reagiereAufTreffer(Location loc){
+        int x = loc.getX();
+        int y = loc.getY();
+        show(1);
+
+        
+    }
+}
